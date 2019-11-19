9c71c0da8af6f90f4ae53aa5c8103ed2fca58154
Scott Pletcher <scott.pletcher@acloud.guru>
2019-11-19 12:00:45 -0500

    Added EPEL repo for Amazon Linux

--- lab-scaling/stress-testing-boot.sh
+++ lab-scaling/stress-testing-boot.sh
@@ -2,5 +2,7 @@
 # USAGE: Place in in the User Data section (under Advanced) when launching an EC2 instance
 # DESCRIPTION: After updating from the repo, installs stress, a tool used to create various system load for testing purposes.
 yum update -y
+wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -P /tmp
+yum install -y /tmp/epel-release-latest-7.noarch.rpm
 yum install stress -y
 /usr/bin/stress --cpu 8 --timeout 10m
